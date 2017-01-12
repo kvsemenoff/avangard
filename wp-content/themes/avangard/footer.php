ffb745b43fe14e00370d840a79eede2480a50f79
unknown <anzarsh@mail.ru>
2017-01-12 15:31:11 +0300

    s

--- wp-content/themes/avangard/footer.php
+++ wp-content/themes/avangard/footer.php
@@ -30,7 +30,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <div class="footer_bottom cf">
-            <div class="wrap">
+            <div class="container">
+            <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
                     <p class="studio" style="margin-left: 0;">© 2009-2016 Мебельная фабрика Авангард<br>  Поддержка сайта - студия «<a href="https://media-bridge.ru/" target="_blank">Media-Bridge</a>»</p>
                 </div>
@@ -39,6 +40,7 @@
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
                     <p class="copyright">Копирование, публикация и использование<br> материалов сайта ЗАПРЕЩЕНЫ!</p>
+                </div>
                 </div>
             </div>
         </div>
