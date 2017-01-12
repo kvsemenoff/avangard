6349916bfa8e9782ed530f612007cf54ae45f7d2
Berzeg07 <berezg86@gmail.com>
2017-01-12 13:48:29 +0300

    29.14

--- wp-content/themes/avangard/header.php
+++ wp-content/themes/avangard/header.php
@@ -15,6 +15,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="stylesheet" href="css/style4.scss">
+<link rel="stylesheet" href="css/style2.scss">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
