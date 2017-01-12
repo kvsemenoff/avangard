c82dc37aa62157af64575cdfeabb061a6b47671a
unknown <anzarsh@mail.ru>
2017-01-12 16:57:27 +0300

    s

--- wp-content/themes/avangard/footer.php
+++ wp-content/themes/avangard/footer.php
@@ -11,13 +11,79 @@
 
 ?>
 
-    </section><!-- #content -->
+</section><!-- #content -->
 
-    <footer id="colophon" class="site-footer" role="contentinfo">
-        <div class="footer_top cf">
-            <div class="wrap">
-                <?php wp_nav_menu( array( 'theme_location' => 'footer-menu_left', 'menu_class' => 'footer-menu_left', 'container' => ''  ) ); ?>
-                <?php wp_nav_menu( array( 'theme_location' => 'footer-menu_right', 'menu_class' => 'footer-menu_right', 'container' => ''  ) ); ?>
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="footer_top cf">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-3 col-xs-12 col-sm-6">
+                   <?php 
+                   $args = array(
+                    'theme_location'  => '',
+                    'menu'            => 'footer1', 
+                    'container'       => false, 
+                    'container_class' => '', 
+                    'container_id'    => '',
+                    'menu_class'      => 'menu', 
+                    'menu_id'         => '',
+                    'echo'            => true,
+                    'fallback_cb'     => 'wp_page_menu',
+                    'before'          => '',
+                    'after'           => '',
+                    'link_before'     => '',
+                    'link_after'      => '',
+                    'items_wrap'      => '<ul>%3$s</ul>',
+                    'depth'           => 0
+                    );
+                   wp_nav_menu($args ); 
+                   ?>
+               </div> 
+               <div class="col-md-3 col-xs-12 col-sm-6">               
+                <?php 
+                $args = array(
+                    'theme_location'  => '',
+                    'menu'            => 'footer2', 
+                    'container'       => false, 
+                    'container_class' => '', 
+                    'container_id'    => '',
+                    'menu_class'      => 'menu', 
+                    'menu_id'         => '',
+                    'echo'            => true,
+                    'fallback_cb'     => 'wp_page_menu',
+                    'before'          => '',
+                    'after'           => '',
+                    'link_before'     => '',
+                    'link_after'      => '',
+                    'items_wrap'      => '<ul>%3$s</ul>',
+                    'depth'           => 0
+                    );
+                wp_nav_menu($args ); 
+                ?>
+            </div>
+             <div class="col-md-3 col-xs-12 col-sm-6">               
+                <?php 
+                $args = array(
+                    'theme_location'  => '',
+                    'menu'            => 'footer3', 
+                    'container'       => false, 
+                    'container_class' => '', 
+                    'container_id'    => '',
+                    'menu_class'      => 'menu', 
+                    'menu_id'         => '',
+                    'echo'            => true,
+                    'fallback_cb'     => 'wp_page_menu',
+                    'before'          => '',
+                    'after'           => '',
+                    'link_before'     => '',
+                    'link_after'      => '',
+                    'items_wrap'      => '<ul>%3$s</ul>',
+                    'depth'           => 0
+                    );
+                wp_nav_menu($args ); 
+                ?>
+            </div>
+            <div class="col-md-3 col-xs-12 col-sm-6">
                 <div class="social">
                     <a href="http://vk.com/club57166671" title="ВКонтакте" class="social_vk"></a>
                     <a href="https://instagram.com/avangard_fm" title="Instagram" class="social_inst"></a>
@@ -25,26 +91,28 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <div class="footer_bottom cf">
-            <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <p class="studio" style="margin-left: 0;">© 2009-2016 Мебельная фабрика Авангард<br>  Поддержка сайта - студия «<a href="https://media-bridge.ru/" target="_blank">Media-Bridge</a>»</p>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <p class="tel">+7 (495) 357-13-00</p>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <p class="copyright">Копирование, публикация и использование<br> материалов сайта ЗАПРЕЩЕНЫ!</p>
-                </div>
-                </div>
+<div class="footer_bottom cf">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <p class="studio" style="margin-left: 0;">© 2009-2016 Мебельная фабрика Авангард<br>  Поддержка сайта - студия «<a href="https://media-bridge.ru/" target="_blank">Media-Bridge</a>»</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <p class="tel">+7 (495) 357-13-00</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                <p class="copyright">Копирование, публикация и использование<br> материалов сайта ЗАПРЕЩЕНЫ!</p>
             </div>
         </div>
-    </footer><!-- #colophon -->
+    </div>
+</div>
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
